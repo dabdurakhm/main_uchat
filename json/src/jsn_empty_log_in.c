dc2e5f967aff73355b1bcfb08204608ade5d9d6b
dvd-ai <56834006+dvd-ai@users.noreply.github.com>
2020-09-29 19:15:25 +0300

    json_empty_log_in

---
+++ json/src/jsn_empty_log_in.c
@@ -0,0 +1,17 @@
+#include "cJSON.h"
+#include "my_json.h"
+
+/*создается пустой json (при входе user-а)*/
+char *jsn_empty_log_in() {  //добавьте этот прототип в общий header!!!
+    cJSON *object = NULL;
+    char *str = NULL;   //вывод
+
+    object = cJSON_CreateObject(); //создается объект object
+    cJSON_AddStringToObject(object, "type", "log_in"); //создается ключ "type" и значение "log_in" в объекте object
+    cJSON_AddStringToObject(object, "user_login", "none");  //создается ключ "user_login" и значение "none" в объекте object
+    cJSON_AddStringToObject(object, "user_password", "none");   //создается ключ "user_password" и значение "none" в объекте object
+
+    str = cJSON_Print(object);  //конвертирую json в строку
+    cJSON_Delete(object);   //удаляю объект user, содержащий свои ключи - значения
+    return str;
+}
